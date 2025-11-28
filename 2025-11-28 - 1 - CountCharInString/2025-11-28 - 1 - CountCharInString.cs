d80eed3d30bc4add66239ccc9e71a16bbbe8d0a1
tim <hallo@timtepasse.de>
2025-11-28 10:39:45 +0100

    Aufgabe 1 a und b

---
+++ 2025-11-28 - 1 - CountCharInString/2025-11-28 - 1 - CountCharInString.cs
@@ -0,0 +1,31 @@
+﻿// 2025-11-29 - 1 - Strings
+
+// Schreiben Sie eine Methode· int CountCharInString(string s, char c), der man einen beliebigen String und einen beliebigen Character übergeben kann. Die Methode soll als Rückgabewert liefern, wie oft der übergebene Character im übergebenen String vorkommt.
+
+#pragma warning disable CS8321 // Local function is declared but never used
+int CountCharInString(string strg, char comparison, bool caseSensitive = false)
+{
+    int sum = 0;
+
+    strg = (!caseSensitive) ? strg : strg.ToLower();
+
+    foreach (char c in strg)
+    {
+        if (c == comparison) { sum++; }
+    }
+
+    return sum;
+}
+#pragma warning restore CS8321 // Local function is declared but never used
+
+
+int CountCharInString2(string strg, char comparison, bool caseSensitive = false)
+{
+    strg = (!caseSensitive) ? strg : strg.ToLower();
+    return strg.Count(c => c == comparison);
+}
+
+
+
+Console.WriteLine(CountCharInString2("Ananas", 'a'));
+Console.WriteLine(CountCharInString2("Ananas", 'a', true));
---
+++ 2025-11-28 - 2 - AnalyseString/2025-11-28 - 2 - AnalyseString.cs
@@ -0,0 +1,35 @@
+﻿// 2025-11-28 - 2 - AnalyseString
+
+// Schreiben Sie eine Methode void AnalyseString(string s), der man einen beliebigen String übergeben kann. Die Methode soll den String analysieren und anschließend auf der Konsole ausgeben wie viele Vokale (a, e, i, o, u), Konsonanten (b, c, d, f, ...), Umlaute (ä, ö, ü, ß), Ziffern (0, 1, 2, 3, ...) und sonstige Zeichen (also der Rest) enthalten waren. Beachten Sie dabei, dass Buchstaben groß oder klein geschrieben sein können.
+// Versuchen Sie zur Bewertung der einzelnen Zeichen die Methode Contains() einzusetzen
+
+
+void AnalyseString(string strg)
+{
+    // string consonants = ;
+    // string umlauts = "äöüß";
+    // string digits = "0123456789";
+
+    uint vocals = 0;
+    uint consonants = 0;
+    uint umlauts = 0;
+    uint digits = 0;
+    uint rest = 0;
+
+    foreach (char c in strg.ToLower())
+    {
+        if      ("aeiou".Contains(c))                 { vocals++; }
+        else if ("bcdfghjklmnpqrstvwxyz".Contains(c)) { consonants++; }
+        else if ("äöüß".Contains(c))                  { umlauts++; }
+        else if ("0123456789".Contains(c))            { digits++; }
+        else                                          { rest++; }
+    }
+
+    Console.WriteLine("Vokale:".PadRight(15) + vocals);
+    Console.WriteLine("Konsonanten:".PadRight(15) + consonants);
+    Console.WriteLine("Umlaute:".PadRight(15) + umlauts);
+    Console.WriteLine("Ziffern:".PadRight(15) + digits);
+    Console.WriteLine("Rest:".PadRight(15) + rest);
+}
+
+AnalyseString("In einer Höhle in der Erde wohnte ein Hobbit.");
