db207a8634fca507c47040380d2f57e4ed0139fb
tim <hallo@timtepasse.de>
2025-11-28 11:30:05 +0100

    IsPalindrom

---
+++ 2025-11-28 - 3 - IsPalindrom/IsPalindrom.cs
@@ -0,0 +1,23 @@
+﻿// 2025-11-28 - 3 - IsPalindrom
+
+// Entwickeln Sie eine Methode bool IsPalindrom(string s) die prüft, ob ein übergebener String ein Palindrom ist. Ein Palindrom ist ein Text, der vorwärts und rückwärts gelesen werden kann. Ignorieren Sie dabei die Groß-/Kleinschreibung.
+
+
+bool IsPalindrom(string strg)
+{
+    strg = strg.ToLower();
+
+    for (int start = 0, end = strg.Length-1; start < end; start++, end--)
+    {
+        if (strg[start] != strg[end])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+Console.WriteLine($"Anna: {IsPalindrom("Anna")}");
+Console.WriteLine($"Otto: {IsPalindrom("Otto")}");
+Console.WriteLine($"Tim: {IsPalindrom("Tim")}");
