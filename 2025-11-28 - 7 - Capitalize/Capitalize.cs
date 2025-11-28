f571f8a665198dfae78f837e5aa51447acc85b51
tim <hallo@timtepasse.de>
2025-11-28 15:17:25 +0100

    Capitalize

---
+++ 2025-11-28 - 7 - Capitalize/Capitalize.cs
@@ -0,0 +1,20 @@
+﻿// schreiben Sie eine Methode string Capitalize(string s), die den übergebenen String in einen String umwandelt, bei dem jedes Wort mit einem Großbuchstaben beginnt. Alle anderen Buchstaben sollen Kleinbuchstaben sein.
+
+// Bsp.:	Capitalize("OTTO")  =>  "Otto"
+//   		Capitalize("hello world!")  =>  "Hello World!"
+
+
+string CapitalizeWord(string s)
+{
+    char[] result = s.ToLower().ToCharArray();
+    result[0] = char.ToUpper(result[0]);
+    return new string(result);
+}
+
+string Capitalize(string s)
+{
+    return String.Join(" ", s.Split(" ").Select(word => CapitalizeWord(word)));
+}
+
+Console.WriteLine($"OTTO          =>  {CapitalizeWord("OTTO")}");
+Console.WriteLine($"hello world!  =>  {Capitalize("hello world!")}");
