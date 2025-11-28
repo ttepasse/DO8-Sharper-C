460c1df79e0e68e4c015ff5446fef1e66a36a08f
tim <hallo@timtepasse.de>
2025-11-28 13:43:24 +0100

    Generierter Stuff, der aus irgendwelchen Grund nicht von der gitignore erfasst wird.

--- 2025-11-28 - 1 - CountCharInString/obj/Debug/net9.0/2025-11-28 - 1 - CountCharInString.AssemblyInfo.cs
+++ 2025-11-28 - 1 - CountCharInString/obj/Debug/net9.0/2025-11-28 - 1 - CountCharInString.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("2025-11-28 - 1 - CountCharInString")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fefcefcc3aefdc9a5d6d67b0bccc347a80eb98b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7de69858e8abc5b6b1fc2d272f32c94b7d8cbae4")]
 [assembly: System.Reflection.AssemblyProductAttribute("2025-11-28 - 1 - CountCharInString")]
 [assembly: System.Reflection.AssemblyTitleAttribute("2025-11-28 - 1 - CountCharInString")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- 2025-11-28 - 2 - AnalyseString/obj/Debug/net9.0/2025-11-28 - 2 - AnalyseString.AssemblyInfo.cs
+++ 2025-11-28 - 2 - AnalyseString/obj/Debug/net9.0/2025-11-28 - 2 - AnalyseString.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("2025-11-28 - 2 - AnalyseString")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f13b39e5f691136ef0c683f35a4c8f515667856c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7de69858e8abc5b6b1fc2d272f32c94b7d8cbae4")]
 [assembly: System.Reflection.AssemblyProductAttribute("2025-11-28 - 2 - AnalyseString")]
 [assembly: System.Reflection.AssemblyTitleAttribute("2025-11-28 - 2 - AnalyseString")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
