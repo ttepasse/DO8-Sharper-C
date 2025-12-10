75c3783f782eae105732d6bad67de6b1de48e505
tim <hallo@timtepasse.de>
2025-12-10 12:17:35 +0100

    LINQ Warenkorb

---
+++ 2025-12-10 - 3 - LINQ Warenkorb/Kunde.cs
@@ -0,0 +1,108 @@
+﻿namespace AufgabeWarenkorb
+{
+    public enum Länder { USA, GB, Germany }
+    class Kunde
+    {
+        public string Name { get; private set; }
+        public string Ort { get; private set; }
+        public Länder Land { get; private set; }
+        public Bestellung[] Bestellungen { get; private set; }
+        public Kunde(string name, string ort, Länder land, Bestellung[] bestellungen)
+        {
+            Name = name;
+            Ort = ort;
+            Land = land;
+            Bestellungen = bestellungen;
+        }
+        public override string ToString()
+        {
+            string result = $"{Name} - {Ort} - {Land}";
+            foreach (Bestellung best in Bestellungen) {
+                result += $"\n  {best}";
+            }
+            return result;
+        }
+        public static Kunde[] GetKundenListe()
+        {
+            return new Kunde[] {
+                new Kunde("Walter", "Altenburg", Länder.Germany, new Bestellung[] {
+                    new Bestellung(2, 4, "März", false)
+                }),
+                new Kunde("Thomas", "Berlin", Länder.Germany, new Bestellung[] {
+                    new Bestellung(1, 11, "Juni", false),
+                    new Bestellung(3, 19, "November", true)
+                }),
+                new Kunde("Holger", "Washington", Länder.USA, new Bestellung[] {
+                    new Bestellung(5, 17, "November", true)
+                }),
+                new Kunde("Fernando", "New York", Länder.USA, new Bestellung[] {
+                    new Bestellung(6, 12, "Juni", false)
+                }),
+                new Kunde("Alice", "London", Länder.GB, new Bestellung[] {
+                    new Bestellung(4, 3, "Februar", true),
+                    new Bestellung(2, 1, "Februar", false),
+                    new Bestellung(3, 19, "Juni", true)
+                })
+            };
+        }
+    }
+    class Bestellung
+    {
+        public int ProduktNr { get; private set; }
+        public int Anzahl { get; private set; }
+        public string Monat { get; private set; }
+        public bool Versendet { get; private set; }
+        public Bestellung(int produktNr, int anzahl, string monat, bool versendet)
+        {
+            ProduktNr = produktNr;
+            Anzahl = anzahl;
+            Monat = monat;
+            Versendet = versendet;
+        }
+        public override string ToString()
+        {
+            return $"ProdNr: {ProduktNr}, Anzahl: {Anzahl}, Monat: {Monat}, Versand: {Versendet}";
+        }
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Bestellung))
+                return false;
+            else
+            {
+                Bestellung b = (Bestellung)obj;
+                return (b.ProduktNr == this.ProduktNr && b.Monat == this.Monat && b.Anzahl == this.Anzahl && b.Versendet == this.Versendet);
+            }
+        }
+        public override int GetHashCode()
+        {
+            return $"{ProduktNr}|{Anzahl}|{Monat}|{Versendet}".GetHashCode();
+        }
+    }
+    class Produkt
+    {
+        public int ProduktNr { get; private set; }
+        public string Name { get; private set; }
+        public decimal Preis { get; private set; }
+        public Produkt(int produktNr, string name, decimal preis)
+        {
+            ProduktNr = produktNr;
+            Name = name;
+            Preis = preis;
+        }
+        public override string ToString()
+        {
+            return $"{ProduktNr} - {Name} - {Preis}";
+        }
+        public static Produkt[] GetProduktListe()
+        {
+            return new Produkt[] {
+                new Produkt(1, "Marmelade", 5),
+                new Produkt(2, "Quark", 10),
+                new Produkt(3, "Mohrrüben", 15),
+                new Produkt(4, "Käse", 20),
+                new Produkt(5, "Honig", 25),
+                new Produkt(6, "Mehl", 30)
+            };
+        }
+    }
+}
---
+++ 2025-12-10 - 3 - LINQ Warenkorb/Program.cs
@@ -0,0 +1,227 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks.Dataflow;
+
+namespace AufgabeWarenkorb;
+
+
+public static class IEnumerableExtension
+{
+    public static void Print<T>(this IEnumerable<T> self)
+    {
+        Console.Write("{");
+        foreach (var element in self.SkipLast(1))
+        {
+            Console.Write($"{element}, ");
+        }
+        Console.WriteLine($"{self.Last()}}}");
+    }
+
+    public static string ToString<T>(this IEnumerable<T> self)
+    {
+        var sb = new StringBuilder("{}");
+        foreach (var element in self.SkipLast(1))
+        {
+            sb.Append($"{element}, ");
+        }
+        sb.Append("}");
+        return sb.ToString();
+    }
+}
+
+class Program
+{
+
+
+    static void Main(string[] args)
+    {
+        // In der Main()-Methode der Klasse Program erzeugen Sie nun zwei Arrays:
+        Kunde[] kunden = Kunde.GetKundenListe();
+        Produkt[] produkte = Produkt.GetProduktListe();
+
+        // In den beiden Klassen Kunde und Produkt ist die ToString()-Methode 
+        // überschrieben, Sie können also die Inhalte der Arrays direkt in 
+        // einer Schleife mit einem Console.WriteLine() ausgeben.
+        // Lösen Sie nun die folgenden Aufgaben. Schreiben Sie jeweils eine 
+        // Lösung in Extension-Method-Syntax und eine in Query-Expression 
+        // Syntax (soweit das möglich/sinnvoll ist).
+
+        Console.Clear();
+
+        // a) Selektieren Sie nur die Namen aller Produkte und anschließend nur Name und Wohnort aller Kunden aus den beiden Arrays.
+        Console.WriteLine("a) Selektieren Sie nur die Namen aller Produkte und anschließend nur Name und Wohnort aller Kunden aus den beiden Arrays.");
+        var resultA1 = from p in produkte
+                       select p.Name;
+        Console.Write("Produkte 1: ");
+        resultA1.Print();
+
+        Console.Write("Produkte 2: ");
+        produkte.Select(p => p.Name).Print();
+
+        var resultA2 = from k in kunden
+                       select $"{k.Name} ({k.Ort})";
+        Console.Write("Name, Wohnort 1: ");
+        resultA2.Print();
+
+        Console.Write("Name, Wohnort 2: ");
+        kunden.Select(k => $"{k.Name} ({k.Ort})").Print();
+
+
+        // b) Selektieren Sie die Bestellungen aller Kunden aus Deutschland.
+        Console.WriteLine("b) Selektieren Sie die Bestellungen aller Kunden aus Deutschland.");
+        var resultB = from k in kunden
+                      where k.Land == Länder.Germany
+                      from b in k.Bestellungen
+                      select $"\"{b}\"";
+        resultB.Print();
+
+        kunden.Where(k => k.Land == Länder.Germany)
+              .SelectMany(k => k.Bestellungen)
+              .Select(b => $"\"{b}\"")
+              .Print();
+
+
+        // c) Selektieren Sie nur den Namen für jeden zweiten Kunden, beginnend mit dem ersten.
+        Console.WriteLine("\nc) Selektieren Sie nur den Namen für jeden zweiten Kunden, beginnend mit dem ersten.");
+        kunden.Where((k, index) => index % 2 == 0).Select(k => k.Name).Print();
+
+
+        //     d) Selektieren Sie nur Name und Preis aller Produkte, die höchstens 20 Euro kosten. Das Ergebnis soll absteigend nach dem Preis sortiert sein.
+        Console.WriteLine("\nd) Selektieren Sie nur Name und Preis aller Produkte, die höchstens 20 Euro kosten. Das Ergebnis soll absteigend nach dem Preis sortiert sein.");
+        var resultD = from p in produkte
+                      where p.Preis <= 20
+                      orderby p.Preis descending
+                      select $"\"{p.Name} ({p.Preis})\"";
+        resultD.Print();
+
+        produkte.Where(p => p.Preis <= 20)
+                .OrderByDescending(p => p.Preis)
+                .Select(p => $"\"{p.Name} ({p.Preis})\"")
+                .Print();
+
+
+        // e) Selektieren Sie nur Name und Land aller Kunden. Das Ergebnis soll aufsteigend nach dem Land sortiert werden. Bei gleichem Land sollen die Kunden nach dem Namen sortiert werden.
+        Console.WriteLine("\ne) Selektieren Sie nur Name und Land aller Kunden. Das Ergebnis soll aufsteigend nach dem Land sortiert werden. Bei gleichem Land sollen die Kunden nach dem Namen sortiert werden.");
+
+        // (Notiz: Die Länder werden nach Enum-Wert sortiert.
+        //  Laut Max einfach ignorieren, statt rumzuarbeiten)
+
+        var resultE = from k in kunden
+                      orderby k.Land, k.Name
+                      select $"\"{k.Name} ({k.Land})\"";
+        resultE.Print();
+
+        kunden.OrderBy(k => k.Land)
+              .ThenBy(k => k.Name)
+              .Select(k => $"\"{k.Name} ({k.Land})\"")
+              .Print();
+
+
+        //     f) Gruppieren Sie die Kunden nach dem Land. Als Gruppenelement soll jeweils das gesamte Kunden-Objekt verwendet werden.
+        Console.WriteLine("\nf) Gruppieren Sie die Kunden nach dem Land. Als Gruppenelement soll jeweils das gesamte Kunden-Objekt verwendet werden.");
+        var resultF = from k in kunden
+                      group k by k.Land;
+        resultF = kunden.GroupBy(k => k.Land);
+        foreach (var group in resultF)
+        {
+            Console.WriteLine(group.Key);
+            foreach (var kunde in group)
+            {
+                Console.WriteLine($"- {kunde.Name} ({kunde.Land})");
+            }
+        }
+
+
+        //     g) Gruppieren Sie die Produkte nach dem ersten Buchstaben des Namens. Als Elemente in den Gruppen sollen nur die Namen der Produkte vorhanden sein.
+        Console.WriteLine("\ng) Gruppieren Sie die Produkte nach dem ersten Buchstaben des Namens. Als Elemente in den Gruppen sollen nur die Namen der Produkte vorhanden sein.");
+        var resultG = from p in produkte
+                      orderby p.Name
+                      group p.Name by p.Name.First();
+
+        resultG = produkte.OrderBy(p => p.Name)
+                          .GroupBy(p => p.Name.First(),
+                                   p => p.Name);
+
+        foreach (var group in resultG)
+        {
+            Console.WriteLine(group.Key);
+            foreach (var kunde in group)
+            {
+                Console.WriteLine($"- {kunde}");
+            }
+        }
+
+
+        //     h) Bilden Sie einen Join zwischen den Bestellungen und den Produkten. Selektieren Sie dann die Werte für Monat, ProduktNr, Name, Preis und Versendet sortiert nach dem Preis.
+        Console.WriteLine("\nh) Bilden Sie einen Join zwischen den Bestellungen und den Produkten. Selektieren Sie dann die Werte für Monat, ProduktNr, Name, Preis und Versendet sortiert nach dem Preis.");
+        var bestellungen = kunden.SelectMany(k => k.Bestellungen);
+
+        var resultH = from b in bestellungen
+                      join p in produkte on b.ProduktNr equals p.ProduktNr
+                      orderby p.Preis
+                      select new { b.Monat, p.ProduktNr, p.Name, p.Preis, b.Versendet };
+
+        resultH = bestellungen
+                    .Join(produkte,
+                          b => b.ProduktNr,
+                          p => p.ProduktNr,
+                          (b, p) => new
+                          {
+                              b.Monat,
+                              p.ProduktNr,
+                              p.Name,
+                              p.Preis,
+                              b.Versendet
+                          })
+                    .OrderBy(x => x.Preis);
+
+
+        foreach (var x in resultH)
+        {
+            Console.WriteLine($"{x.ProduktNr} {x.Name} ({x.Preis} Geld), bestellt: {x.Monat}, versendet {x.Versendet}");
+        }
+
+
+        //     i) Selektieren Sie alle Kunden mit Name, Wohnort und Anzahl Bestellungen.
+        Console.WriteLine("\ni) Selektieren Sie alle Kunden mit Name, Wohnort und Anzahl Bestellungen.");
+        var resultI = from k in kunden
+                      select new
+                      {
+                          Name = k.Name,
+                          Ort = k.Ort,
+                          AnzahlBestellungen = k.Bestellungen.Length
+                      };
+        resultI.Print();
+
+
+        // j) Summieren Sie die Preise aller Produkte aus der Produktliste.
+        Console.WriteLine("\nj) Summieren Sie die Preise aller Produkte aus der Produktliste.");
+        Console.WriteLine(produkte.Sum(p => p.Preis));
+
+
+        // k) Selektieren Sie alle Kunden mit ihrem Namen und dem Gesamtbetrag ihrer Bestellungen.
+        Console.WriteLine("\nk) Selektieren Sie alle Kunden mit ihrem Namen und dem Gesamtbetrag ihrer Bestellungen.");
+        var resultK = from k in kunden
+                      select new {
+                        Name = k.Name,
+                        Summe = (from b in k.Bestellungen
+                                 join p in produkte on b.ProduktNr equals p.ProduktNr
+                                 select b.Anzahl * p.Preis).Sum()
+                      };
+        
+        resultK = kunden.Select(k => new
+        {
+            Name = k.Name,
+            Summe = k.Bestellungen.Join(produkte,
+                                        b => b.ProduktNr,
+                                        p => p.ProduktNr,
+                                        (b, p) => b.Anzahl * p.Preis).Sum()
+        });
+
+        foreach(var kunde in resultK)
+        {
+            Console.WriteLine($"{kunde.Name}    {kunde.Summe}");
+        }
+
+    }
+}
